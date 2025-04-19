792d8ace6b41c365c749399559a464fbe86fba84
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-04-19 21:49:38 +0300

    Complete event handling lab with Keypad and EyesOnMe components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Keypad from './components/Keypad';
+import EyesOnMe from './components/EyesOnMe';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Event Handling Lab</h1>
+      <Keypad />
+      <br />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
